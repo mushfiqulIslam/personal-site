e3ab9e020e15729701b7b06a43bb43d91d369a4d
mushfiqulIslam <nnahid878@gmail.com>
2022-02-11 16:35:11 +0600

    initialized nextjs project

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <html>
+    <body>
+    
+    <h1>My First Heading</h1>
+    <p>My first paragraph.</p>
+    
+    </body>
+    </html> 
+  )
+}
+
+export default Home
+
