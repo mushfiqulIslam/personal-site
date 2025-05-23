61deda9d4625cbeab14cf8b82c8eb2cee100db8f
mushfiqulIslam <nnahid878@gmail.com>
2025-05-23 20:53:48 +0600

    writing deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -5,6 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/",  // Change this to your repository name if not deploying to a custom domain
   server: {
     host: "0.0.0.0",
     port: 8080,
